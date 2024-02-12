492e65487fb549707a7269638c727eae30b67d1a
gangbeomsu <kbs21c1987@gmail.com>
2024-02-12 21:00:52 +0900

    variants를 사용해서 커스텀하기

--- project/src/components/Modal.jsx
+++ project/src/components/Modal.jsx
@@ -2,13 +2,20 @@ import { motion } from 'framer-motion';
 import { createPortal } from 'react-dom';
 
 export default function Modal({ title, children, onClose }) {
+  // const hiddenAnimationState = { opacity: 0, y: 30 };
+
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, y: 30 }} // 초기 상태를 지정한다.
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }} // 애니메이션이 끝날때
+        variants={{
+          // 키 이름은 원하는데로 지정할 수 있다.
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden" // 초기 상태를 지정한다.
+        animate="visible"
+        exit="hidden" // 애니메이션이 끝날때
         open
         className="modal"
       >
