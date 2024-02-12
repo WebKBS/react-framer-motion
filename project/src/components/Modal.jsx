c6ce1d503c75a6c759ba02c6461fd613760bdb42
gangbeomsu <kbs21c1987@gmail.com>
2024-02-12 20:47:18 +0900

    modal, exit으로 이벤트 끝나면 닫기

--- project/src/components/Modal.jsx
+++ project/src/components/Modal.jsx
@@ -8,6 +8,7 @@ export default function Modal({ title, children, onClose }) {
       <motion.dialog
         initial={{ opacity: 0, y: 30 }} // 초기 상태를 지정한다.
         animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }} // 애니메이션이 끝날때
         open
         className="modal"
       >
